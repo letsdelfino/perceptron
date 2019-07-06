359a9fd3e6c806557e1b624f05eadce01740723a
letsdelfino <41598982+letsdelfino@users.noreply.github.com>
2019-07-06 00:04:13 -0300

    atualizando para arquivo final

--- Classificação de vinhos/code.py
+++ Classificação de vinhos/code.py
@@ -1,19 +1,75 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-# Import area
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn.model_selection import train_test_split # functions for machine learning
-from subprocess import check_output
+#importando bibliotecas
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+from subprocess import check_output #mostra o arquivo da database seno usado
+import pandas as pd #usada para manipulação de arquivos
+#import tensorflow as tf
+import numpy as np #uado para criar array
+import keras
+from keras import optimizers
+from keras.optimizers import Adam, rmsprop, sgd
+from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.layers import Dense, Activation
 
-#import os
-#print(os.listdir("../input"))
-
-# Any results you write to the current directory are saved as output.
-
-#print data file
+#mostrando arquivo que está sendo usado
 print(check_output(["ls", "../input"]).decode("utf8"))
+
+#incianco leitura do arquivo
+dados = pd.read_csv('../input/Wine.csv', header=None)
+dados.columns = ['Classe','Álcool','Ácido málico','Cinza','Alcalinidade das cinzas','Magnésio','totalPhenols','Fenóis totais','Fenol não flavonóide' ,'Proantocianimas','Intensidade da cor','Tom','OD280/OD315','prolineProlina']
+print(dados)
+
+#eliminando a primeira coluna lida do arquivo
+X_entrada = dados.drop(['Classe'], axis=1)
+Y_saida = dados.iloc[:,:1] #seleciona linhas e colunas por números 
+#https://medium.com/horadecodar/data-science-tips-02-como-usar-loc-e-iloc-no-pandas-fab58e214d87
+
+#convertendo dados para array
+#devem ser duas matrizes, uma com as características e outra com as classes. As saídas devem ser passadas para uma matriz onde sua representação será feita com zeros e uns
+X = X_entrada.values
+Y = Y_saida.values
+
+#fazendo a verificação da conversão
+print(type(X), type(Y))
+
+#criando a representação das classes
+def classificador(classificacao):
+    if classificacao == 1:
+        return [1, 0, 0] #equivalente a classe um
+    if classificacao == 2:
+        return [0, 1, 0] #equivalente a classe dois
+    if classificacao == 3:
+        return [0, 0, 1] #equivalente a classe três
+      
+ #salvando novas valores gerados pela função de classificação criada no array Y
+
+Y = np.array([classificador(i[0]) for i in Y])
+
+#verificando tamanho dos conjuntos de treino
+print(X.shape)
+print(Y.shape)
+
+#criando modelo
+model = Sequential([
+    Dense(12, input_dim=13, activation='relu'),
+    Dense(8, input_dim=13, activation='relu'),
+    Dense(3, activation='softmax')
+              ])
+
+#compilando modelo
+
+sgd = optimizers.SGD(lr=0.001)
+model.compile(
+  optimizer='sgd',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+#treinando a rede usando o arquivo todo
+model.fit(X,Y,epochs=1000, batch_size=178)
+
+predictions = model.predict(X)
+print("predictions",predictions[0])
+
+score = model.evaluate(X,Y,verbose=0)
+print(score)
